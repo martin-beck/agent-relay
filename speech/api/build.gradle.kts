97be8028d38a2660ae1ade27bb334d2fd59bd408
martin-beck <24471267+martin-beck@users.noreply.github.com>
2026-09-02 00:57:26 +0200

    feat(speech): define offline service contract
    Signed-off-by: martin-beck <24471267+martin-beck@users.noreply.github.com>

---
+++ speech/api/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    api(libs.kotlinx.coroutines.core)
+
+    testImplementation(kotlin("test"))
+    testCompileOnly(libs.junit)
+}
