5ccddbe932b5a91559ef89d66dcee0d1271e91be
martin-beck <24471267+martin-beck@users.noreply.github.com>
2026-09-05 02:23:41 +0200

    ci: test emulator without GPU

--- scripts/ci/run_android_emulator_tests.sh
+++ scripts/ci/run_android_emulator_tests.sh
@@ -73,7 +73,7 @@ cleanup() {
   fi
 }
 trap cleanup EXIT
-setsid "$emulator_bin" -port "$EMULATOR_PORT" -avd "$EMULATOR_AVD_NAME" -no-window -gpu swiftshader_indirect -no-snapshot -no-audio -no-boot-anim > "$log_file" 2>&1 < /dev/null &
+setsid "$emulator_bin" -port "$EMULATOR_PORT" -avd "$EMULATOR_AVD_NAME" -no-window -gpu off -no-snapshot -no-audio -no-boot-anim > "$log_file" 2>&1 < /dev/null &
 emulator_pid=$!
 echo "$emulator_pid" > "$pid_file"
 echo "Started emulator pid=$emulator_pid port=$EMULATOR_PORT log=$log_file"
