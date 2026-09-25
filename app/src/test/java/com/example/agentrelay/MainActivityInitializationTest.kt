8c653ccbdf5c9211a76c60986ab68aca19399cc1
Martin Beck <martin.beck2@gmx.de>
2026-09-25 17:58:10 +0200

    fix(android): initialize settings after activity attach
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

---
+++ app/src/test/java/com/example/agentrelay/MainActivityInitializationTest.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.example.agentrelay
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
+class MainActivityInitializationTest {
+    @Test
+    fun activityCanBeConstructedBeforeContextIsAttached() {
+        Robolectric.buildActivity(MainActivity::class.java).get()
+    }
+}
